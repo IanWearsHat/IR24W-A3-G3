a9416e244d0f372d931317c4264464d6bbcfb881
Ian Dai <ianbbdai@gmail.com>
2024-03-05 01:22:00 -0800

    Partial indexes created by putting small inv_index into separate json files

--- indexer/Milestone1.py
+++ indexer/Milestone1.py
@@ -16,7 +16,7 @@ Making actual index:
         - the dict in value has keys are document ID, values are lists
             - the list first has another list of the position of that term in the document
                 - implicitly has the number of times the term occurs in the document (take len of lists of positions)
-            - the second elem in the list is the tf score for the document
+            - the second elem in the list is the number of docs token appears in
             - the third elem is the tf-idf score
                 - when all files have been parsed, go through each term and calculate the idf score
 
@@ -24,12 +24,12 @@ Making actual index:
         "token": {
             docID: [
                 [3, 4, 8, ... , position],
-                term_frequency,
+                number of docs token appears in,
                 tf_idf_score -----------------# calculated in update_tf_idf_scores
             ],
             docID2: [
                 [other positions],
-                term_frequency,
+                number of docs token appears in,
                 tf_idf_score
             ]
         }
@@ -50,6 +50,7 @@ class Indexer:
     docID_map = dict()
 
     docID_count = 0
+    index_count = 0
 
     def __init__(self, dir_name, index_file_name, docID_file_name):
         self._dir_name = dir_name
@@ -89,17 +90,20 @@ class Indexer:
             file_content = json.load(fr)  # load json file
             url = file_content["url"]  # extract url
             content = file_content["content"]  # extract content
+
+            # to find a specific url
+            if url.strip() == "https://www.informatics.uci.edu/xmlrpc.php?rsd":
+                print(file_path)
+                print()
+
             text = BeautifulSoup(
-                content, features="xml"
+                content, features="lxml"
             ).get_text()  # parse html contents
 
-            # TODO: potential optimization: removing hyphens, periods, paranthese, etc.
-            # keep in mind the complexities involved. ex. co-chair needs to be together and can't be split
             text = re.sub(r"[\W_]+", " ", text)
             split_text = text.split()
             for i in range(len(split_text)):  # iterate the word in the text
-                word = split_text[i]
-                # token = self.stemmer.stem(word)
+                word = split_text[i].lower()
 
                 one_file_word_freq.setdefault(word, list())
 
@@ -132,7 +136,7 @@ class Indexer:
 
                 url, one_file_map = self._get_one_file_token_freq(file_path)
 
-                if (self.docID_count % 1000 == 0):
+                if self.docID_count % 1000 == 0:
                     print(self.docID_count, url)
 
                 self._update_inv_index(one_file_map)
@@ -140,9 +144,16 @@ class Indexer:
 
                 self.docID_count += 1
 
-        self._update_tf_idf_scores()
+                # dump current inv_index into json file and reset inv_index
+                if self.docID_count % 30 == 0:
+                    self._write_dict_to_file(self.inv_index, str(self.index_count) + ".json")
+                    self.inv_index = {}
+                    self.index_count += 1
 
-        self._write_dict_to_file(self.inv_index, self._index_file_name)
+        # self._update_tf_idf_scores()
+
+        # one final partial index for anything left over
+        self._write_dict_to_file(self.inv_index, str(self.index_count) + ".json")
         self._write_dict_to_file(self.docID_map, self._docID_file_name)
 
         os.chdir(self.orig_dir)
@@ -182,7 +193,7 @@ def cosine_similarity(vec1, vec2):
 
 
 if __name__ == "__main__":
-    is_test = False
+    is_test = True
 
     if is_test:
         directory = "ANALYST"
@@ -192,8 +203,20 @@ if __name__ == "__main__":
     indexer = Indexer(
         directory, directory + "_inv_index.json", directory + "_doc_ID_map.json"
     )
+    import time
+
+    past = time.time()
+
     indexer.create_index()
-    print("Document Count:", indexer.get_document_count())
+    # url, dic = indexer._get_one_file_token_freq("44a45c3cfd95b0299d3ac374bd3ec48a08d5402cdfda3d87cfb61ba55c37d74c.json")
+    # print(dic)
+
+    new = time.time()
+    time_taken = new - past
+    doc_count = indexer.get_document_count()
+    print(time_taken, "seconds taken")
+    print(time_taken / doc_count, "seconds per doc on average")
+    print(doc_count, "documents parsed")
 
     # # Define a list of test queries
     # test_queries = [
@@ -208,3 +231,56 @@ if __name__ == "__main__":
     #     print(f"Processing query: {query}")
     #     result_docs = indexer.process_query(query)
     #     print(f"Documents intersection found: {result_docs}\n")
+
+    # TODO: does this need to be filtered out? http://computableplant.ics.uci.edu/models/Activator/WU-Activator-Update-2010/vertices.tsv
+    # TODO: switch to pathlib instead
+"""
+Creating partial indexes:
+- create a partial index file for every 30 documents
+
+Merging:
+- For each file, get a term and its posting dict
+    - if the term is in the discovered set, move on to the next term
+
+    - t1 = {
+        0: {
+            [3, 4, 5], the position of the term in the doc
+            3,  # number of times the token appears in the doc
+            9   # number of words in the doc
+        },
+        1: {
+            [6, 7, 10], the position of the term in the doc
+            3,  # number of times the token appears in the doc
+            15   # number of words in the doc
+        },
+    }
+
+- now for each file again except the current one and all ones previous to it, see if the term is in the file and get its posting, if not, continue to the next file
+    - t2 = {
+        3: {
+            [3, 4, 5], the position of the term in the doc
+            3,  # number of times the token appears in the doc
+            9   # number of words in the doc
+        },
+        6: {
+            [6, 7, 10], the position of the term in the doc
+            3,  # number of times the token appears in the doc
+            15   # number of words in the doc
+        },
+    }
+
+- simply add all items from t2 into t1
+
+- do this for every term for all files until we reach the end of the last file
+
+- add t1 to discovered set.
+
+- note that for each nested loop to find t2, you only need to look for every file onwards. 
+    - ex. partial indexes 5, 4, 3, 2, 1
+        if we are looking at 4 and t1 is in 4, we only need to look at 3, 2, 1
+
+- _update_tf_idf_scores() will need to be run on each file after creating the inv index initially
+        
+- somehow have indexes for the indexes
+    - like knowing that 1.json has terms from a-c, although everything is alphanumeric so idk
+"""
