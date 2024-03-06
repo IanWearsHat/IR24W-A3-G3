b6158da3a7afae7684bb86feddcb2e9df1e1e242
Ian Dai <ianbbdai@gmail.com>
2024-03-06 12:22:28 -0800

    Implemented merging partial indexes

--- api/src/index.py
+++ api/src/index.py
@@ -60,11 +60,40 @@ class Index:
 
 
 if __name__ == "__main__":
-    index = Index("DEV_inv_index.json", "DEV_doc_ID_map.json")
-    input_str = input("Input a query: ")
-    while (input_str != "exit"):
-        doc_ids = index.get_query_intersection(input_str)
-        urls = index.get_top_urls(doc_ids)
-        for url in urls[:5]:
-            print(url)
-        input_str = input("Input a query: ")
+    # index = Index("DEV_inv_index.json", "DEV_doc_ID_map.json")
+    # input_str = input("Input a query: ")
+    # while (input_str != "exit"):
+    #     doc_ids = index.get_query_intersection(input_str)
+    #     urls = index.get_top_urls(doc_ids)
+    #     for url in urls[:5]:
+    #         print(url)
+    #     input_str = input("Input a query: ")
+    import re
+    import timeit
+
+    def alphanumeric_iterator(input_string):
+        current_sequence = ''
+        for char in input_string:
+            if char.isalnum():
+                current_sequence += char
+            elif current_sequence:
+                yield current_sequence
+                current_sequence = ''
+        if current_sequence:
+            yield current_sequence
+
+    # Define the input string
+    input_string = "abc 902-took]bruh b" * 100000  # Repeat for larger input
+    iterator = alphanumeric_iterator(input_string)
+
+    # Define the regular expression pattern
+    pattern = re.compile(r'\w+')
+
+    # Benchmark using regular expressions
+    regex_time = timeit.timeit(lambda: list(pattern.finditer(input_string)), number=100)
+
+    # Benchmark without regular expressions
+    no_regex_time = timeit.timeit(lambda: [s for s in iterator], number=100)
+
+    print("Time taken with regular expressions:", regex_time)
+    print("Time taken without regular expressions:", no_regex_time)
